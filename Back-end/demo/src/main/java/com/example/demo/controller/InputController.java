76dc693269b286c8d800df63efc030d5d280c287
Sofia <2115658102@qq.com>
2024-11-12 20:09:50 +0800

    the opretion of back-end

---
+++ Back-end/demo/src/main/java/com/example/demo/controller/InputController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.InputService;
+import com.example.demo.entity.InputDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class InputController {
+
+    // 显示输入页面
+   @GetMapping
+    public String hello(){return"hello world";}
+
+    @Autowired
+    private InputService inputService;
+
+    @GetMapping("/list")
+    public List<InputDate> list(){
+        return inputService.list();
+    }
+    }
+
---
+++ Back-end/demo/src/main/java/com/example/demo/entity/InputDate.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class InputDate {
+       public String batteryName;
+       public String batteryType;
+}
