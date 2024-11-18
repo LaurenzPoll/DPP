af61fd08bc195d9ce482569ce02b42faf8a8c0b0
employee-management-project_0 <1134918747@qq.com>
2024-11-18 23:13:36 +0800

    Remove unnecessary files

--- front yalen/test/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // 引入刚刚创建的路由配置
-
-createApp(App)
-    .use(router) // 使用 Vue Router
-    .mount('#app');
