b9cd9e2b3f2cde7e8bdaaed28b6c235f459aa55d
employee-management-project_0 <1134918747@qq.com>
2024-11-18 22:02:57 +0800

    1

---
+++ front yalen/homepage/src/components/QuickScanResults.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="results-page">
+    <h2>Quick scan results</h2>
+    <p>Here your quick scan results will be displayed.</p >
+    <!-- 这里可以展示扫描结果的内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuickScanResults'
+};
+</script>
+
+<style scoped>
+.results-page {
+  text-align: center;
+  padding: 20px;
+}
+</style>
