2f38dac425d97e8f396d6da179accff7dc692180
LaurenzPoll <laurenzvandepoll@icloud.com>
2024-11-20 08:09:14 +0100

    Moved all vue files one level up in the directory

--- front yalen/src/main.js
+++ front yalen/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // 引入刚刚创建的路由配置
+import router from './router/index.js'; // 引入刚刚创建的路由配置
 
 createApp(App)
     .use(router) // 使用 Vue Router
