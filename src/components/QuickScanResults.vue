5249380c01d7af6f63c920aec17d69540bf97181
employee-management-project_0 <1134918747@qq.com>
2024-11-20 15:08:57 +0800

    清空 yalen 分支并提交 frontyalen 目录中的新内容

---
+++ src/components/QuickScanResults.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="results-page">
+    <h2>快速扫描结果</h2>
+    <p>这里将显示您的快速扫描结果。</p >
+    <!-- 这里可以展示扫描结果的内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuickScanResults'
+};
+</script>
+
+<style scoped>
+.results-page {
+  text-align: center;
+  padding: 20px;
+}
+</style>
